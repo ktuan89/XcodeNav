ac1cbfc7f3c878fa64c9a066f5b7193bece6bdda
Anh Khuc <anhk@fb.com>
2014-07-11 00:05:59 -0700

    Use template project

---
+++ XcodeNav/XcodeNav/XNXcodeNav.h
@@ -0,0 +1,13 @@
+//
+//  XNXcodeNav.h
+//  XNXcodeNav
+//
+//  Created by Anh Khuc on 7/11/14.
+//  Copyright (c) 2014 Anh Khuc. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface XNXcodeNav : NSObject
+
+@end
